26be5da7108440d52a30bc6f0433ed7647278c35
jordan <jwald3@brockport.edu>
2023-07-03 20:38:34 -0400

    Initial Commit

---
+++ BeginnerTicTacToe/BeginnerTicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginnerTicTacToeApp.swift
+//  BeginnerTicTacToe
+//
+//  Created by Jordan Riesel on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BeginnerTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
